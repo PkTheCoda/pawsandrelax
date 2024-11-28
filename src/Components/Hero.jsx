5d7a831cd05c2116a335cecd90f831c7aaaa8cf4
PkTheCoda <pranavkonjeti@gmail.com>
2024-11-28 11:01:03 -0500

    services page done

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,20 +2,30 @@ import React from 'react'
 import dogmain from '../Images/dogmain.png'
 import { IoPaw } from "react-icons/io5";
 
+import doghappyicon from '../Images/icons/doghappyicon.png'
+import dogicon from '../Images/icons/dogicon.png'
+import staricon from '../Images/icons/staricon.png'
+
+
 const Hero = () => {
   return (
-    <div className='bg-slate-100 py-20 font-cute'>
+    <div className='bg-slate-100 py-20 font-cute relative'>
+
+      <div className='h-[12rem] w-full bg-slate-200 absolute bottom-0'>
+
+      </div>
+
       <div className='max-w-[95rem] mx-auto flex flex-col gap-y-4 relative'>
 
         {/* <IoPaw className='absolute w-40 h-40 right-0 -rotate-45 text-slate-200 top-1/4'/> */}
 
         <div className='py-10 text-black flex flex-col items-center gap-y-4 text-center'>
           <h1 className='font-medium eight-title tracking-tighter z-40 relative'>
-            Your Dog's <br />
+            Dog Grooming <br />
             <span className='relative'>
-              Second Home
+              Done with Care
             </span>
-            <IoPaw className='absolute w-14 h-14 text-slate-300 -top-6 -rotate-45 left-10 -z-10'/>
+            <IoPaw className='absolute w-14 h-14 text-slate-300 -top-6 -rotate-45 left-0 -z-10'/>
             <IoPaw className='absolute w-14 h-14 text-slate-300 bottom-0 rotate-45 -right-6 -z-10'/>
           </h1>
           <p className='one-title tracking-wide leading-9 max-w-2xl relative'>
@@ -28,6 +38,46 @@ const Hero = () => {
 
         <div className=''>
           <img src={dogmain} alt="Picture of a dog" className='relative'/>
+        </div>
+
+        <div className='py-10 mt-20 w-full max-w-[62rem] mx-auto grid grid-cols-3 gap-4'>
+
+          <div className='bg-white rounded-lg shadow-lg w-full flex items-center gap-6 px-4 py-8 -rotate-1'>
+            <img src={staricon} alt="" className='h-16 w-16'/>
+            <div className=''>
+              <h4 className='five-title'>
+                4.9/5
+              </h4>
+              <p className='text-slate-700'>
+                Average Rating by Pet Parents Across Richmond
+              </p>
+            </div>    
+          </div>
+
+          <div className='bg-white rounded-lg shadow-lg w-full flex items-center gap-6 px-4 py-8 rotate-1'>
+            <img src={dogicon} alt="" className='h-16 w-16'/>
+            <div className=''>
+              <h4 className='five-title'>
+                400+
+              </h4>
+              <p className='text-slate-700'>
+                Dogs and Pets groomed in our facility
+              </p>
+            </div>    
+          </div>
+
+          <div className='bg-white rounded-lg shadow-lg w-full flex items-center gap-6 px-4 py-8 -rotate-1'>
+            <img src={doghappyicon} alt="" className='h-16 w-16'/>
+            <div className=''>
+              <h4 className='five-title'>
+                98%
+              </h4>
+              <p className='text-slate-700'>
+                of customers said they would be coming back
+              </p>
+            </div>    
+          </div>
+
         </div>
 
       </div>
--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import dogservices from '../Images/dogservices.png'
 
 const Services = () => {
   return (
-    <div className='bg-slate-200 py-10 font-cute pb-80'>
-        <div className='max-w-[95rem] mx-auto py-10 space-y-2'>
+    <div className='bg-slate-200 pb-10 font-cute'>
+        <div className='max-w-[90rem] mx-auto pb-10 space-y-4'>
             <div className='flex flex-col items-center gap-y-2 text-center'>
                 <h2 className='eight-title tracking-tight'>
                     Our Services
@@ -12,6 +13,105 @@ const Services = () => {
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. In perferendis nihil, dolorum cumque tempora excepturi 
                 </p>
             </div>
+
+            <div className='py-10 grid grid-cols-10'>
+                <div className="py-10 col-span-3 flex flex-col justify-around">
+
+                    <div className='flex gap-4 items-center'>
+                        <h2 className='seven-title text-main'>
+                            01
+                        </h2>
+                        <div className='flex flex-col'>
+                            <h2 className='four-title'>
+                                Full Groom & Style
+                            </h2>
+                            <p className='text-slate-700'>
+                                A head-to-tail grooming session with a bath, haircut, nail trim, ear cleaning, and finishing touches.
+                            </p>
+                        </div>  
+                    </div>
+
+                    <div className='flex gap-4 items-center'>
+                        <h2 className='seven-title text-main'>
+                            02
+                        </h2>
+                        <div className='flex flex-col'>
+                            <h2 className='four-title'>
+                                Puppy’s First Spa Day
+                            </h2>
+                            <p className='text-slate-700'>
+                                Gentle grooming for your pup's first visit, designed to create a stress-free and fun experience.
+                            </p>
+                        </div>  
+                    </div>
+
+                    <div className='flex gap-4 items-center'>
+                        <h2 className='seven-title text-main'>
+                            03
+                        </h2>
+                        <div className='flex flex-col'>
+                            <h2 className='four-title'>
+                            Shedding Solution Treatment
+                            </h2>
+                            <p className='text-slate-700'>
+                                A specialized de-shedding service to reduce shedding, promote a healthy coat, and keep your home fur-free.
+                            </p>
+                        </div>  
+                    </div>
+
+                </div>
+
+                <div className="py-10 col-span-4">
+                    <img src={dogservices} alt="" />
+                </div>
+
+                <div className="py-10 col-span-3 flex flex-col justify-around">
+                    
+                    <div className='flex gap-4 items-center'>
+                        <h2 className='seven-title text-main'>
+                            04
+                        </h2>
+                        <div className='flex flex-col'>
+                            <h2 className='four-title'>
+                                Creative Grooming
+                            </h2>
+                            <p className='text-slate-700'>
+                                Add flair with pet-safe dye, glitter accents, or stencil art for a truly unique look.
+                            </p>
+                        </div>  
+                    </div>
+
+                    <div className='flex gap-4 items-center'>
+                        <h2 className='seven-title text-main'>
+                            05
+                        </h2>
+                        <div className='flex flex-col'>
+                            <h2 className='four-title'>
+                                Aromatherapy Bath
+                            </h2>
+                            <p className='text-slate-700'>
+                                A calming bath with essential oils to relax your dog and leave them smelling fresh and fabulous.
+                            </p>
+                        </div>  
+                    </div>
+
+                    <div className='flex gap-4 items-center'>
+                        <h2 className='seven-title text-main'>
+                            06
+                        </h2>
+                        <div className='flex flex-col'>
+                            <h2 className='four-title'>
+                                Pawdicure Package
+                            </h2>
+                            <p className='text-slate-700'>
+                                Includes a nail trim, paw balm application, and a paw massage to keep those paws healthy and soft.
+                            </p>
+                        </div>  
+                    </div>
+
+                </div>
+            </div>
+
         </div>
     </div>
   )
